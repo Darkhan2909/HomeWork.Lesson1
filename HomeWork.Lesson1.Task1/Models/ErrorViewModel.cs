84cb67c3277df5d6f7a62d58cd67d79905c0a58b
ГерценЕ <@223-P>
2023-05-15 19:28:47 +0600

    Добавьте файлы проекта.

---
+++ HomeWork.Lesson1.Task1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeWork.Lesson1.Task1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
